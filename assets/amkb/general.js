9b0d2303cbcfa67ff3640c3ab9a33f3785716dd9
HeadStarterz <89839353+HeadStarterz@users.noreply.github.com>
2023-07-03 15:24:20 +0200

    Update general.js

--- assets/amkb/general.js
+++ assets/amkb/general.js
@@ -1,3 +1,20 @@
+// protect emails from crawler
+$(function() {
+    $('a[href^="mailto:"]').each(function() {
+        var linkText = this.innerHTML;
+        var href = this.href;
+
+        if (linkText.includes('[at]') && linkText.includes('[dot]')) {
+            var updatedLinkText = linkText.replace('[at]', '@').replace(/\[dot\]/g, '.');
+            this.innerHTML = updatedLinkText;
+        }
+
+        var newHref = href.replace('[at]', '@').replace(/\[dot\]/g, '.');
+        this.href = "mailto:" + newHref.replace('mailto:', '');
+    });
+});
+
+// change staging domains to slug
 $(document).ready(function() {
     $('a[href]').each(function() {
         var href = $(this).attr('href');
